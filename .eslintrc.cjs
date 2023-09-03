d7d4b049eaa5b9b623a16f60eb7f270f58bf9a99
Not Thomiz <jeanouina@gmail.com>
2023-09-03 19:35:56 +0200

    enable eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
         "no-empty": ["error", {
             allowEmptyCatch: true
         }],
-        "@typescript-eslint/no-this-alias": "off"
+        "@typescript-eslint/no-this-alias": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
     }
 };
